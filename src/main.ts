638ac0f634303378aa00be422c107ba4621d456c
Yanislav Igonin <yanislavdabest@gmail.com>
2020-06-10 22:22:45 +0300

    update: app port

--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { config } from './config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(config.port);
 }
 bootstrap();
-
-/**
- * GET /boards - list of all boards
- * GET /boards/:boardId - board info
- *
- * GET /boards/:boardId/threads?page=1 - threads list
- * POST /boards/:boardId/threads - create thread
- *
- * GET /boards/:boardId/threads/:threadId/posts - posts for thread
- * POST /boards/:boardId/threads/:threadId/posts - create post in thread
- */
-
-/**
- * GET /boards - list of all boards
- * GET /boards/:boardId - board info
- * GET /threads?board=b&page=1 - threads list
- * POST /threads { boardId: b, ...} - create thread
- * GET /posts?board=b&thread=1 - posts for thread
- * POST /posts { boardId: b, threadId: 1, ...} - create post in thread
- */
