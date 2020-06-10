58678c9fe3b059edeadaf225f3760f4b29edd272
Yanislav Igonin <yanislavdabest@gmail.com>
2020-06-10 22:50:50 +0300

    update: add typeorm init

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { RouterModule, Routes } from 'nest-router';
 import { ThreadsModule } from './threads/threads.module';
 import { BoardsModule } from './boards/boards.module';
 import { PostsModule } from './posts/posts.module';
+import { config } from './config';
 
 const routes: Routes = [
   {
@@ -25,6 +27,14 @@ const routes: Routes = [
 
 @Module({
   imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: config.postgresUrl,
+      entities: ['**/*.entity{.ts}'],
+      migrationsTableName: 'migrations',
+      migrations: ['src/migrations/*.ts'],
+      ssl: config.env === 'production',
+    }),
     RouterModule.forRoutes(routes),
     BoardsModule,
     ThreadsModule,
