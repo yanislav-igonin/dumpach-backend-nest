9ef928bb85650980dd4ddb8775366e9123541347
Yanislav Igonin <yanislavdabest@gmail.com>
2020-06-10 15:36:21 +0300

    add: create thread dto

---
+++ src/threads/dto/create-thread.dto.ts
@@ -0,0 +1,15 @@
+import { IsString, IsBoolean, IsArray } from 'class-validator'
+
+export class CreateThreadDto {
+  @IsString()
+  title: string;
+
+  @IsString()
+  text: string;
+
+  @IsBoolean()
+  isSage: boolean;
+
+  @IsArray()
+  files: string[];
+}
