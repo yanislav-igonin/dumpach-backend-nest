8bf1274e071bbc1ecef643eb9d2cc675147feb43
Yanislav Igonin <yanislavdabest@gmail.com>
2020-06-12 02:45:03 +0300

    update: change mocks on repo

--- src/threads/threads.service.ts
+++ src/threads/threads.service.ts
@@ -1,23 +1,35 @@
 import { Injectable } from '@nestjs/common';
-import { threads } from './mocks';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { ThreadEntity } from './entities';
+import { BoardsService } from '../boards/boards.service';
 
 @Injectable()
 export class ThreadsService {
-  private readonly threads = threads;
+  constructor (
+    @InjectRepository(ThreadEntity)
+    private threadsRepository: Repository<ThreadEntity>,
+    // @InjectRepository(BoardEntity)
+    private boardsService: BoardsService,
+  ) {}
 
   async getThreads(boardId: string, pageId: number): Promise<ThreadEntity[]> {
-    const threads = this.threads[boardId].sort((t1, t2) => {
-      if (t1.updatedAt > t2.updatedAt) return 1;
-      if (t1.updatedAt < t2.updatedAt) return -1;
-      return 0;
+    // const board = this.boardsRepository.findOne(boardId);
+
+    const threads = await this.threadsRepository.find({
+      where :{ 
+        boardId,
+      },
+      order: {
+        updatedAt: 'DESC',
+      }
     });
 
-    return threads.slice(10 * pageId, 10 * pageId + 10);
-  }
+    return threads;
+  };
 
-  async createThread(boardId: string, data: ThreadEntity): Promise<ThreadEntity> {
-    this.threads[boardId].push(data);
-    return data;
-  }
+  // async createThread(boardId: string, data: ThreadEntity): Promise<ThreadEntity> {
+  //   // this.threads[boardId].push(data);
+  //   return ThreadEntity;
+  // }
 }
