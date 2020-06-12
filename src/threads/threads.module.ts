92421bd7db0d4d3e26c8e53626bcd9e57c40ee3f
Yanislav Igonin <yanislavdabest@gmail.com>
2020-06-12 13:58:48 +0300

    update: add board check on threads get

--- src/threads/threads.module.ts
+++ src/threads/threads.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { BoardEntity } from '../boards/entities';
 import { ThreadsController } from './threads.controller';
 import { ThreadsService } from './threads.service';
 import { ThreadEntity } from './entities';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ThreadEntity])],
+  imports: [TypeOrmModule.forFeature([ThreadEntity, BoardEntity])],
   controllers: [ThreadsController],
   providers: [ThreadsService],
 })
--- src/threads/threads.service.ts
+++ src/threads/threads.service.ts
@@ -1,20 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { BoardEntity } from '../boards/entities';
 import { ThreadEntity } from './entities';
 
 @Injectable()
 export class ThreadsService {
   constructor (
+    @InjectRepository(BoardEntity)
+    private boardsRepository: Repository<BoardEntity>,
     @InjectRepository(ThreadEntity)
     private threadsRepository: Repository<ThreadEntity>,
   ) {}
 
   async getThreads(boardId: string, pageId: number): Promise<ThreadEntity[]> {
-    // const board = this.boardsRepository.findOne(boardId);
+    const board = await this.boardsRepository.findOne(boardId);
+
+    if (board === undefined) throw new NotFoundException('Board Not Found');
 
     const threads = await this.threadsRepository.find({
-      where :{ 
+      where: { 
         boardId,
       },
       order: {
