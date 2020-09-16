e808c05cc444ab9c2da5d40b1e61e00da48a0392
Yanislav Igonin <yanislavdabest@gmail.com>
2020-09-16 18:51:04 +0300

    feat: now running app not in docker

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 const config = {
   env: process.env.NODE_ENV || 'development',
   port: process.env.PORT ? parseInt(process.env.PORT) : 3000,
-  postgresUrl: process.env.POSTGRES_URL || 'localhost/dumpach',
+  postgresUrl: process.env.POSTGRES_URL || 'postgres://dumpach:test_password@localhost/dumpach',
 };
 
 export { config };
